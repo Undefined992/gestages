f1bd9dac2e0dd4e46458c5e6ea195968cd7e5f78
Undefined992 <150731613+Undefined992@users.noreply.github.com>
2024-06-14 10:10:47 +0200

    Update f_bdd.php

--- include/function/f_bdd.php
+++ include/function/f_bdd.php
@@ -4,7 +4,13 @@ function connexionBDD()
 {
 	try
 	{
-            $bdd = new PDO('mysql:host=servbd;port=3306;dbname=bdd_geststages;charset=utf8', 'usergs', 'mdpGS', array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
+            	$conn = mysqli_init();
+		mysqli_ssl_set($conn,NULL,NULL, "/var/www/html/DigiCertGlobalRootCA.crt.pem", NULL, NULL);
+		mysqli_real_connect($conn, 'mydemoserver.mysql.database.azure.com','myadmin', 'yourpassword', 'quickstartdb', 3306, MYSQLI_CLIENT_SSL);
+		if (mysqli_connect_errno()) {
+			die('Failed to connect to MySQL: '.mysqli_connect_error());
+		}
+	$bdd = new PDO('mysql:host=servbd;port=3306;dbname=bdd_geststages;charset=utf8', 'usergs', 'mdpGS', array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
             return $bdd;
 	}
 	catch(Exception $e)
