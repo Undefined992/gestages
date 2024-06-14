2bf310e7f9cf55c3fe5c38fc597301cf252de361
Undefined992 <150731613+Undefined992@users.noreply.github.com>
2024-06-14 11:47:23 +0200

    Update f_bdd.php

--- include/function/f_bdd.php
+++ include/function/f_bdd.php
@@ -1,23 +1,29 @@
-﻿<?php
+<?php
 
 function connexionBDD()
 {
-	try
-	{
-            	$conn = mysqli_init();
-		mysqli_ssl_set($conn,NULL,NULL, "/var/www/html/DigiCertGlobalRootCA.crt.pem", NULL, NULL);
-		mysqli_real_connect($conn, 'bdd-tp-mysql-jordan.mysql.database.azure.com','jordan', 'undefined.fr', 'quickstartdb', 3306, MYSQLI_CLIENT_SSL);
-		if (mysqli_connect_errno()) {
-			die('Failed to connect to MySQL: '.mysqli_connect_error());
-		}
-	$bdd = new PDO('mysql:host=servbd;port=3306;dbname=bdd_geststages;charset=utf8', 'usergs', 'mdpGS', array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
+    try
+    {
+        $options = array(
+            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+            PDO::MYSQL_ATTR_SSL_CA => 'DigiCertGlobalRootCA.crt.pem' // Chemin vers le certificat RootCA de Azure
+            );
+    
+            $bdd = new PDO(
+            'mysql:host=bdd-tp-mysql-jordan.mysql.database.azure.com;port=3306;dbname=bdd_geststages;charset=utf8',
+            'jordan',
+            'undefined.fr',
+            $options
+        );
+            //array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
+        
             return $bdd;
-	}
-	catch(Exception $e)
-	{
-		$pdo_error = $e->getMessage();
+    }
+    catch(Exception $e)
+    {
+        $pdo_error = $e->getMessage();
                 return false;
-	}
+    }
     
 }
 
